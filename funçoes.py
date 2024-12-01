8c936e4eeb6e31072136e42944eccf5135cf8bd7
rafelt412 <rafelt444@gmail.com>
2024-12-01 09:15:41 +0000

    aprimoramento

--- funçoes.py
+++ funçoes.py
@@ -1,1 +1,85 @@
+import pandas as pd  # Importa a biblioteca pandas
 
+
+def ler():
+  """
+  Função que lê o arquivo json e o reutiliza em outras funções
+  Args: Nenhum.
+  Returns: Dicionário com a chave pet e o valor o animal e a chave food e o valor a comida
+      exemplo:  {"pet": "cachorro", "food": "ração"}
+  """
+  df = pd.read_json('diretorio.json', orient='records')  # Lê o arquivo json
+  return df  # Retorna o dicionário
+
+
+def animal_filtro():
+  """
+  Função que filtra os animais com base na entrada do usuário
+  Args: Nenhum.
+  Returns: Nome de um animal
+           animal: str
+  """
+  animal = input('Digite o nome do animal: ')
+  return animal
+
+
+def verificação(
+    animal
+):  # Verifica se a entrada do usuário é válida e pega o animal da função animal_filtro
+  """
+  Função que verifica se a entrada do usuário é válida e pega o animal da função animal_filtro
+  Args: return da função animal_filtro
+        animal: str
+  Retorns: True se a entrada for válida, e False se for inválida
+  """
+  df = ler()  # Lê o arquivo json
+  if animal in df['pet'].values:  # Verifica se o animal está no arquivo json
+    return True  # Retorna True se a entrada for válida
+  else:
+    return False  # Retorna False se a entrada for inválida
+
+
+def filtrar_animal(animal):  # Filtra os animais com base na entrada do usuário(função animal_filtro))
+  """
+  Esta Fução filtra os animais com base na entrada do usuário(função animal_filtro)
+  Args: return da função animal_filtro
+        animal: str
+  Returns: Retorna uma lista com os animais que contém o mesmo nome da entrada do usuário
+          exemplo: ['cachorro'] ['cachorro', 'gato']']
+          filtrar_animal(cachorro)
+          ['cachorro','cachorro']
+  """
+  verificar = verificação(animal)  # Verifica se a entrada do usuário é válida
+  if verificar is True:  # Se a entrada for válida
+    df = ler()  # Lê o arquivo json
+    df_animal = df[df['pet'] == animal]  # Filtra os animais com base na entrada do usuário
+    return print(df_animal)
+  else:
+    raise KeyError('Animal não encontrado')
+
+
+def filtrar_animal_completo(
+):  #Melhorar essa função encapsulando outras funções para que fique mais limpo e robusto
+  animal = animal_filtro()
+  filtrar_animal(animal=animal)
+
+
+def comida_filtro():
+  comida = animal_filtro()
+  return comida
+
+
+def verificação_comida(comida):
+  df = ler()
+  if comida in df['food'].values:
+    return True
+  else:
+    raise ValueError('Comida não encontrada')
+
+
+def filtrar_comida(comida):
+  verificar = verificação_comida(comida)
+  if verificar is True:
+    df = ler()
+    df_comida = df[df['food'] == comida]
+    return print(df_comida)
--- main.py
+++ main.py
@@ -2,39 +2,86 @@ import pandas as pd
 
 save = []
 
+
 def validar_entrada(entrada):
   """
-  Função que valida a entrada do usuário.
+  Função que valida a entrada do usuário, para a validação do animal e comida
+  Ela recebe uma entrada do usuário e retorna True se a entrada for válida, e False se for inválida
   Args:
-    entrada (str): Entrada do usuário.
+    entrada (str): entrada do usuário (animal ou comida)
+    Obs: Uso uma função para pedir os dados do usuário e validar a entrada
   Returns:
-    bool: True se a entrada for válida, False caso contrário.
+    bool: True se a entrada for válida, e Retorna um erro caso seja inválida
+    Obs: Aqui tem muitas mensagens de erro, podendo reaprovei-las para o usuário
+  Exemplo De Uso:
+    >>> def função():  # Função que recebe a entrada do usuário
+    >>> animal = 'cachorro' # "Entrada do usuário"
+    >>> validar_entrada(entrada= animal)  # Valida a entrada do usuário
+    >>> return True  # Retorna True se a entrada for válida
+    >>> except Valueerror as e: # Caso a entrada seja inválida, retorna um erro
+    >>> print(e)
   """
-  if len(entrada) < 3:
+  if len(entrada) < 3:  # Valida se a entrada é menor que 3 caracteres
     raise ValueError(
         'Entrada inválida, o número de caracteres deve ser maior ou igual a 3')
-  if entrada[0] == ' ':
+  if entrada[0] == ' ':  # Valida se a entrada começa com um espaço
     raise ValueError(
         'Entrada inválida, o primeiro caractere não pode ser um espaço')
-  if entrada[-1] == ' ':
+  if entrada[-1] == ' ':  # Valida se a entrada termina com um espaço
     raise ValueError(
         'Entrada inválida, o último caractere não pode ser um espaço')
-  if entrada.count(' ') <= 3:
+  if entrada.count(' ') > 3:  # Válida se a entrada possui mais de 3 espaços
     raise ValueError(
-        'Entrada inválida, o número de espaços deve ser menor ou igual a 3')
-  if entrada[0].isuuper():
+        'Espaços inválidos, a entrada deve conter no máximo 3 espaços')
+  if entrada[0].isuper():  # Valida se a entrada começa com letra maiúscula
     raise ValueError(
         'Entrada inválida, o primeiro caractere deve ser minúsculo')
-  if isinstance(entrada, int):
+  if isinstance(entrada, int):  # Valida se a entrada é um número
     raise ValueError('Entrada inválida, o valor deve ser uma string')
-  return True  
+  return True
+
 
 def criar_animal_comida():
   """
   Função que cria um animal e sua comida
   Args: Nenhum.
-  Returns: Dicionário com a chave pet e o valor o animal e a chave food e o valor a comida.
+  Essa função cria argumentos para outras funções.
+  Returns: Dicionário com a chave pet e o valor o animal e a chave food e o valor a comida
+  E erro caso a entrada seja inválida seja aniaml ou comida
   """
-  animal = input('Digite o nome do animal: ')
-  comida = input('Digite o nome da comida: ')
-  ...
+  try:
+    animal = input('Digite o nome do animal: ')
+    validar_entrada(entrada=animal)
+    comida = input('Digite o nome da comida: ')
+    validar_entrada(entrada=comida)
+    return {'pet': animal, 'food': comida}
+  except ValueError as e:
+    print(e)
+
+
+def stop():
+  go = input('[P]arar?').strip().upper()
+  if go == 'P':
+    return True
+  else:
+    return False
+
+
+def loop_criar_animal_comida():
+  while True:
+    try:
+      criar = criar_animal_comida()
+      global save
+      save.append(criar)
+      df = pd.DataFrame(save)
+      df.to_json('diretorio.json', orient='records')
+      if stop() is True:
+        break
+      else:
+        continue
+    except ValueError as e:
+      print(e)
+      if stop() is True:
+        break
+      else:
+        continue
