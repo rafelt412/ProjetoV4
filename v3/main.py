b892c9fbe5d61c350aa306414d6776b737d6b21c
Rafael Ferreira Torres <rafelt440@gmail.com>
2024-12-07 11:52:18 -0400

    atualizaçao

--- v3/main.py
+++ v3/main.py
@@ -7,3 +7,6 @@ save = []
 def criar_animal_comida():
     animal = input("Digite o nome de um animal: ")
     comida = input("Digite o nome de uma comida: ")
+    return animal,comida
+def validar():
+    ...
