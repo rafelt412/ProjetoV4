0725b2e74d5b471524f3118cb37397396dbf490e
Rafael Ferreira Torres <rafelt440@gmail.com>
2024-12-07 12:21:19 -0400

    atualizaçao

--- v3/main.py
+++ v3/main.py
@@ -3,10 +3,20 @@ import os
 
 save = []
 
+def clear():
+    os.system('clear')
 
 def criar_animal_comida():
     animal = input("Digite o nome de um animal: ")
+    validar_entrada(entrada= animal)
     comida = input("Digite o nome de uma comida: ")
     return animal,comida
-def validar():
-    ...
+def validar_entrada(entrada):
+    while True:
+        if not entrada:
+            clear()
+            print(f'Não deixe o nome do(a) {entrada} em branco!')
+            continue
+        if len(entrada) < 3:
+            ...
+        return True
