ae40cbe01256e5776e4cf9f834fe43af2e494e1c
Rafael Ferreira Torres <rafelt440@gmail.com>
2024-12-08 03:55:28 -0400

    atualizaçao

--- v3/main.py
+++ v3/main.py
@@ -7,13 +7,13 @@ save = []
 def clear():
     """
     Função que limpa o terminal
-    Args=  
+    Args=
         nenhum.
     Returns=
         limpa o terminal
     """
     os.system('clear')
-    
+
 
 def criar_animal_comida():
     """
@@ -24,8 +24,18 @@ def criar_animal_comida():
     Returns=
         Tupla (animal,comida):str
     """
-    animal = input("Digite o nome de um animal: ")
-    comida = input("Digite o nome de uma comida: ")
+    while True:
+        animal = input("Digite o nome de um animal: ")
+        if validar_entrada(entrada=animal, tipo='animal') is False:
+            continue
+        else:
+            break
+    while True:
+        comida = input('Digite o nome de uma comida para o animal descrito: ')
+        if validar_entrada(entrada=comida, tipo='comida') is False:
+            continue
+        else:
+            break
     return animal, comida
 
 
@@ -34,19 +44,38 @@ def validar_entrada(entrada, tipo):
     Função que valida a entrada de dados do usuário
     na parte de "criação" do animal e a comida
     Args=
-        (entrada):str 
+        (entrada):str
     Returns=
         (animal):str e (comida):str
     """
     while True:
         try:
-            if isinstance(entrada,str) and not entrada.strip():
+            if not isinstance(entrada, str):
                 clear()
-                print(f'Erro: não deixe espaços em branco no(a) {tipo}')
-                continue
-            float_entrada = float(entrada)
-            if float_entrada is False():
+                raise ValueError('erro: digite somente letras')
+            if isinstance(entrada, str) and not entrada.strip():
                 clear()
-                print(f'Erro: não digite 0 no(a) {tipo}')
-        except:
-            pass
+                raise NameError(
+                    f'Erro: não deixe espaços em branco no(a) {tipo}')
+            try:
+                float_entrada = float(entrada)
+                if float_entrada == 0:
+                    clear()
+                    print(f'Erro: não digite 0 no(a) {tipo}')
+                    return False
+                if float_entrada != 0:
+                    clear()
+                    print(f'Erro: não digite numeros no(a) {tipo}')
+                    return False
+            except ValueError:
+                return True
+        except NameError as e:
+            clear()
+            print(f'{e}')
+            return False
+        except ValueError as e:
+            clear()
+            f'{e}'
+            return False
+a = criar_animal_comida()
+print(a)
